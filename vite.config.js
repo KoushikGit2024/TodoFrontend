3a8c05ad039a3545f90f6e82237a6f613e0c5697
Koushik Kar <koushikkar712@gmail.com>
2025-11-02 14:47:17 +0530

    Fix final1

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ 
+  plugins: [
     tailwindcss(),
-    react()
+    react(),
   ],
   server: {
-    historyApiFallback:true,
+    port: 5173,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
 })
